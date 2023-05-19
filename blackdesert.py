54aeb97cfad28034e49cc1bf9df779eea5aa66d8
pureullee <pureullee@gmail.com>
2023-05-20 01:39:35 +0900

    직접 요리를 하는 경우의 비용을 추정하는 함수 구현

--- blackdesert.py
+++ blackdesert.py
@@ -1,23 +1,51 @@
 
 import pandas as pd
 import tkinter as tk
-
+import numpy as np
+proficiency = 3
 
 def addRecipe() :
-    global df
+    
     inputValue = maincookEntry.get()
     inputPrice = mainCookPriceEntry.get()
-    df.loc[len(df)] = [inputValue] + [(value.get()) for pair in zip(subCookEntries, subCookQuaEntries) for value in pair] +[inputPrice] #pair가 양 값을 가지고 그 pair를 value로 풀어서 get
+    last=len[df] 
+    df.loc[last] = [inputValue] + [(value.get()) for pair in zip(subCookEntries, subCookQuaEntries) for value in pair] +[inputPrice] #pair가 양 값을 가지고 그 pair를 value로 풀어서 get
     df.loc[df['target']==inputValue, 'price'] = mainCookPriceEntry.get() # 같은 타겟이면 전부 똑같은 가격 설정
+    df.loc[last]['iscook'] = 1 if df.loc[last]['sub1'] != None else 0 #1은 요리, 0은 원재료
     print("저장 성공")
     print(df.tail())
     
 def setPrice() :
-    global df
+    
     inputValue = maincookEntry.get()
     df.loc[df['target']==inputValue, 'price'] = mainCookPriceEntry.get() # 같은 타겟이면 전부 똑같은 가격 설정
     print("setPirce")
     print(df[df['target']==inputValue])
+    
+def tracePrice(target) :
+    
+    # 매개변수 target의 값이 target 시리즈에 있는 행에 대해서 iscook 값이 0인, 즉 원재료인경우라면 그 원재료 가격의 평균을 리턴함
+    if df.loc[df['target'] == target,'iscook'].iloc[0] == 0 :
+        return df.loc[df['target']== target,'price'].mean()
+    
+    # iscook 값이 1인경우엔, sub1 부터 sub5 까지 순환하며 다시 재귀적으로 함수를 콜함
+    else :
+        #하위 요리명만을 가진 데이터 프레임을 생성함
+        newDf = df.loc[df['target'] == target, ['sub1','sub2','sub3','sub4','sub5']]
+        
+        #newDf의 길이만큼 각 행에 대해 sub1~sub5까지의 하위 재료를 각각 재귀함수에 넣음
+        
+        for i in range(len(newDf)) :
+            cost = 0
+            for v in newDf.iloc[i] :
+                if v is not np.nan  :
+                    cost += tracePrice(v) 
+            cost //= proficiency
+            
+            df.loc[df['target'] == target, 'cost'] = cost
+            print(df.loc[df['target'] == target, 'cost'])
+                    
+            
     
 
 fileName = 'recipe.xlsx' 
@@ -26,17 +54,15 @@ try :
     df = pd.read_excel(fileName,)
     
 except FileNotFoundError : 
-    df = pd.DataFrame(columns=['target','sub1','sub1_qua','sub2','sub2_qua','sub3','sub3_qua','sub4','sub4_qua','sub5','sub5_qua','price'], )
+    df = pd.DataFrame(columns=['target','sub1','sub1_qua','sub2','sub2_qua','sub3','sub3_qua','sub4','sub4_qua','sub5','sub5_qua','price', 'cost' 'iscook'], )
 print(df)
+
+tracePrice('채소 볶음')
     
 window = tk.Tk()
 window.title("Black Desert Calculation")
 window.geometry("1080x480+200+200")
 window.resizable(False, False)
-
-
-            
-        
 
 top =tk.Label(window, text="황실 납품 이익 최대화 관련 프로그램입니다.\n insert 기능으로 상품의 조합식을 추가 할 수 있습니다. ", width=0, height=0,)
 top.pack()
@@ -95,7 +121,6 @@ insertButton = tk.Button(window,text ="AddRecipe", command=addRecipe )
 setPriceButton = tk.Button(window, text = "setPrice", command=setPrice)
 insertButton.pack()
 setPriceButton.pack()
-
 window.mainloop()
 df.to_excel(fileName,index=False ) #remove index  
     
